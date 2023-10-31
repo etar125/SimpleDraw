bcea88c03a4b0f6f8c35fb1f2203f25f9634de0f
etar125 <116297277+etar125@users.noreply.github.com>
2023-10-31 14:37:38 +0300

    0.04
    Добавлена возможность рисовать пиксельные изображения(типа растягивать их без потери качества).

---
+++ simpledraw/Objects/Visual/PixelPicture.cs
@@ -0,0 +1,22 @@
+﻿// Created by etar125 in SharpDevelop 5.1
+using System;
+using System.Drawing;
+
+namespace simpledraw.Objects.Visual
+{
+	public class PixelPicture
+	{
+		public Point Location;
+		public Image Image;
+		public Size newSize;
+		
+		public PixelPicture() { }
+		
+		public PixelPicture(Point location, Image image, Size newsize)
+		{
+			Location = location;
+			Image = image;
+			newSize = newsize;
+		}
+	}
+}
